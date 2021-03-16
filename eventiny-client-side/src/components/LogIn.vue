c24465d7b9da3f36c141b06bbee90c3b1e2c8d12
ZribiHaythem <zribihaythem@gmail.com>
2021-03-16 10:46:39 +0100

    user-dashbord-added

--- eventiny-client-side/src/components/LogIn.vue
+++ eventiny-client-side/src/components/LogIn.vue
@@ -123,7 +123,7 @@ export default {
               return;
             } else if (data.token !== undefined) {
               localStorage.setItem("token", data.token);
-              this.$router.push("/");
+              this.$router.push("/GeneralPage");
             }
           })
           .catch((err) => {
