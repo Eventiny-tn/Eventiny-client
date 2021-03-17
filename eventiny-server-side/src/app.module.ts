ec4e527f0aefbd0a531da7cb3a4abfb267e3a461
ZribiHaythem <zribihaythem@gmail.com>
2021-03-17 11:04:50 +0100

    staging

--- eventiny-server-side/src/app.module.ts
+++ eventiny-server-side/src/app.module.ts
@@ -5,9 +5,11 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { EventssModule } from './events/eventss.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, EventssModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- eventiny-server-side/src/user/user.entity.ts
+++ eventiny-server-side/src/user/user.entity.ts
@@ -29,7 +29,7 @@ export class User {
   @Column({ default: 0 })
   reportCounter: number;
   @Column()
-  userimg:string;
+  userimg: string;
 }
 
 export interface Userinfo {
