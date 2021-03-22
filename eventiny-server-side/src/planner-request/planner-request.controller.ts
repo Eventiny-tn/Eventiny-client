251bdb0b4903136e3465e0c8c9da3d0c7b4d6bf7
ALAAMEKKI <alaamekki01@gmail.com>
2021-03-22 19:55:38 +0100

    eventdetails

---
+++ eventiny-server-side/src/planner-request/planner-request.controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@nestjs/common';
+import { PlannerRequestService } from './planner-request.service';
+
+@Controller('planner-request')
+export class PlannerRequestController {
+  constructor(private readonly plannerRequestService: PlannerRequestService) {
+    
+
+  }
+}
